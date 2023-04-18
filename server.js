989c6e5b37b91591a61af6103485016fb39d4160
Steph-en <appahstephen9@gmail.com>
2023-04-18 15:27:34 +0000

    database chnge

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
+const ejs = require('ejs');   
+const bodyparser = require('body-parser');
+const mysql = require('mysql');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
 const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
@@ -13,10 +16,23 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 // Set static folder
-// app.use(express.static(__dirname, 'public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const botName = 'Chat Bot';
+
+// Database Connection
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  // port: '3306',
+  user: 'root',
+  password: '',
+  database: 'SchoolDB'
+})
+
+connection.connect(function(error){
+  if(!!error) console.log(error);
+  else console.log('Database Connected!')
+})
 
 // Run when a client connects
 io.on('connection', socket => {
